44317afed02098965b3e169a5f64a284e8e38ac9
fuadayodeji <fuadayodeji@gmail.com>
2023-06-11 07:28:21 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,5 +17,46 @@ resource "azurerm_subnet" "subnet" {
     virtual_network_name = azurerm_virtual_network.vnet.name
     resource_group_name = "azurerm_resource_group.rg.name"
     address_prefixes = "10.0.10.0/24"
-  
+}
+resource "azurerm_network_interface" "nic" {
+    name = "book-nic"
+    location = "West Europe"
+    resource_group_name = azurerm_resource_group.rg.name
+    ip_configuration {
+        name = "bookipconfig"
+        subnet_id = azurerm_subnet.subnet.id
+        private_ip_address_allocation = "Dynamic"
+        public_ip_address_id = azurerm_public_ip.pip.id
+
+    }
+
+}
+resource "azurerm_public_ip" "pip" {
+    name = "book-ip"
+    location = "West Europe"
+    resource_group_name = "${azurerm_resource_group.rg.name}"
+    public_ip_address_allocation = "Dynamic"
+    domain_name_label = "bookdevops"
+}
+resource "azurerm_storage_account" "stor" {
+    name = "bookstor"
+    location = "West Europe"
+    resource_group_name = azurerm_resource_group.rg.name
+    account_tier = "Standard"
+    account_replication_type = "LRS"
+}
+resource "azurerm_linux_virtual_machine" "vm" {
+    name = "bookvm"
+    location = "West Europe"
+    resource_group_name = azurerm_resource_group.rg.name
+    vm_size = "Standard_DS1_v2"
+    network_interface_ids = ["${azurerm_network_interface.nic.
+id}"]
+storage_image_reference {
+    publisher = "Canonical"
+    offer = "UbuntuServer"
+    sku = "16.04-LTS"
+    version = "latest"
+}
+
 }
